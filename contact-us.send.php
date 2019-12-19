ae7b6f559fbc54eac29627dd1803af550f5183e0
vanhnl <46739257+vanhnl@users.noreply.github.com>
2019-12-20 01:18:36 +0700

    Assignment 2

---
+++ contact-us.send.php
@@ -0,0 +1,12 @@
+<?php
+	$dbhost='localhost';
+	$db=''
+	$fullname='root';
+	$email='';
+	$telephone='';
+	$message='';
+
+	mysqli_connect("$dbhost","$fullname","$email","$telephone","$message");
+	echo"asdjkf";
+	mysqli_select_db($db);
+?>
